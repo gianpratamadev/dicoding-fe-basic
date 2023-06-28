4a735b9d3ace34483862b69a830b055cf90bea96
Gian Pratama <gianpratama.dev@gmail.com>
2023-06-28 10:26:09 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const carousel = document.querySelector(".carousel");
+
+firstImg = carousel.querySelectorAll("img")[0];
+arrowIcons = document.querySelectorAll(".top-content i");
+
+let isDragStart = false,
+  prevPageX,
+  prevScrollLeft;
+
+const showHiddenIcons = () => {
+  let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+  arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+  arrowIcons[1].style.display =
+    carousel.scrollLeft == scrollWidth ? "none" : "block";
+};
+
+arrowIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let firstImgWidth = firstImg.clientWidth + 14;
+    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+    setTimeout(() => showHiddenIcons(), 60);
+  });
+});
+
+const dragStart = (e) => {
+  isDragStart = true;
+  prevPageX = e.pageX || e.touched[0].pageX;
+  prevScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) => {
+  if (!isDragStart) return;
+  e.preventDefault();
+  carousel.classList.add("dragging");
+  let positionDiff = e.pageX - prevPageX;
+  carousel.scrollLeft = prevScrollLeft - positionDiff;
+  showHiddenIcons();
+};
+
+const dragStop = () => {
+  isDragStart = false;
+  carousel.classList.remove("dragging");
+};
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
+carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
+carousel.addEventListener("touchend", dragStop);
